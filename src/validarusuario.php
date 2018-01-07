227133b285177635ed7bc75ae6f6d494d31c2472
Kevin <ken_1721@hotmail.es>
2018-01-07 12:14:38 -0500

    avanzando

--- src/validarusuario.php
+++ src/validarusuario.php
@@ -6,8 +6,7 @@
  $password =$_POST["password"];
  //echo "string";
   $objColector= new UsuarioCollector();
- echo $nombre;
- echo $password;
+
    $usuario=$objColector->consUsuario("$nombre","$password");
    if($usuario->getNombre() and $usuario->getPassword()){
    	echo "aceptado";
