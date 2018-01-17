dad47e7e278d710869613d4a7575c4cb493d4898
Kevin <ken_1721@hotmail.es>
2018-01-17 10:29:56 -0500

    administrador y leer usuarios

--- admin/index.php
+++ admin/index.php
@@ -4,3 +4,237 @@ if ( $_SESSION["perfil"]!= "admin"){
 	header("location: ../index.php");
 }
 echo "Bienvenido admin";
+?>
+<a href="usuario/index.php">Usuarios</a>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Simple Responsive Admin</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+     
+           
+          
+    <div id="wrapper">
+         <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="adjust-nav">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img src="../img/logo.gif" />
+
+                    </a>
+                    
+                </div>
+              
+                <span class="logout-spn" >
+                  <a href="../pages/logout.php" style="color:#fff;">LOGOUT</a>  
+
+                </span>
+            </div>
+        </div>
+        <!-- /. NAV TOP  -->
+        
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-lg-12">
+                     <h2>ADMINISTRADOR</h2>   
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+                <div class="row">
+                    <div class="col-lg-12 ">
+                        <div class="alert alert-info">
+                             <strong>Bienvenido <?php echo $_SESSION['nombre'];?> ! </strong> 
+                        </div>
+                       
+                    </div>
+                    </div>
+                  <!-- /. ROW  --> 
+                            <div class="row text-center pad-top">
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-circle-o-notch fa-5x"></i>
+                      <h4>Check Data</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div> 
+                 
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-envelope-o fa-5x"></i>
+                      <h4>Mail Box</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-lightbulb-o fa-5x"></i>
+                      <h4>New Issues</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="usuario/index.php" >
+ <i class="fa fa-users fa-5x"></i>
+                      <h4>Ver Usuarios</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-key fa-5x"></i>
+                      <h4>Admin </h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-comments-o fa-5x"></i>
+                      <h4>Support</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+              </div>
+                 <!-- /. ROW  --> 
+                <div class="row text-center pad-top">
+                 
+                 <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-clipboard fa-5x"></i>
+                      <h4>All Docs</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-gear fa-5x"></i>
+                      <h4>Settings</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-wechat fa-5x"></i>
+                      <h4>Live Talk</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-bell-o fa-5x"></i>
+                      <h4>Notifications </h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-rocket fa-5x"></i>
+                      <h4>Launch</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div>
+                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+                      <div class="div-square">
+                           <a href="blank.php" >
+ <i class="fa fa-user fa-5x"></i>
+                      <h4>Register User</h4>
+                      </a>
+                      </div>
+                     
+                     
+                  </div> 
+              </div>   
+                  <!-- /. ROW  -->    
+        
+                 <!-- /. ROW  -->  
+                 <div class="row text-center pad-top">
+                 
+                     
+                     
+                  </div>
+               
+				  <div class="row">
+                    <div class="col-lg-12 ">
+					<br/>
+                        <div class="alert alert-danger">
+                             <strong>Want More Icons Free ? </strong> Checkout fontawesome website and use any icon <a target="_blank" href="http://fortawesome.github.io/Font-Awesome/icons/">Click Here</a>.
+                        </div>
+                       
+                    </div>
+                    </div>
+                  <!-- /. ROW  --> 
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+    <div class="footer">
+      
+    
+           
+        </div>
+          
+
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+    
+   
+</body>
+</html>
