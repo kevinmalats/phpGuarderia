00859dd8eb7fd76404e1aca8b55728ff2329a0b8
davidrobelly <david.robelly@gmail.com>
2018-01-27 11:55:17 -0500

    agregando dia

---
+++ src/collector_dia.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once('dia.php');
+include_once('database/collector.php');
+
+class DiaCollector extends Collector
+{
+  
+  function showDias() {
+    $rows = self::$db->getRows("SELECT * FROM dia_horario ");        
+    $arrayDia= array();        
+    foreach ($rows as $c){
+      $aux = new Dia();
+      $aux->setId($c{'id_dia'});
+      $aux->setNombre($c{'nombre'});
+      $aux->setHoraInicio($c{'hora_inicio'});
+      $aux->setHoraFIn($c{'hora_fin'});
+      $aux->setHorarioIdHorario($c{'horario_id_horario'});
+      array_push($arrayDia, $aux);
+    }
+    return $arrayDia;        
+  }
+  
+function updateDia($id_dia,$nombre, $horario_inicio, $horario_fin, $horario_id_horario) {
+    $insertrow = self::$db->updateRow("UPDATE public.dia_horario SET nombre = ?, horario_inicio = ?, horario_fin = ? , horario_id_horario = ?, where id_dia= ? ", array ("{$nombre}","{$hora_inicio}","{$hora_fin}","{$hora_id_horario}",$id_dia));
+
+}
+function deleteDia($id) {
+    $deleterow = self::$db->deleteRow("DELETE FROM public.dia_horario where id_dia= ? ", array ("{$id}"));
+
+}
+function insertDia($nombre, $hora_inicio, $hora_fin, $horario_id_horario) {
+    $rows = self::$db->insertRow("INSERT INTO public.dia_horario(nombre, horario_inicio, hora_fin, horario_id_horario) VALUES (?,?,?,?) returning id_dia", array ("{$nombre}","{$hora_inicio}","{$hora_fin}", "{$horario_id_horario}"));      
+    return $rows{"id_dia"};      
+  }
+
+7
+
+
+}
+?>
---
+++ src/dia.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* 
+*/
+class Dia {
+
+private $id_dia;
+private $nombre ;
+private $hora_inicio;
+private $hora_fin;
+private $horario_id_horario;    
+
+
+public function __construct(){
+   
+
+}
+public function setId($id_dia){
+     $this->id_dia=$id_dia;
+}
+public function getId(){
+	return $this->id_dia;
+}
+public function setNombre($var){
+     $this->nombre=$var;
+}
+public function getNombre(){
+	return $this->nombre;
+}
+public function setHoraInicio($var){
+     $this->hora_inicio=$var;
+}
+public function getHoraInicio(){
+	return $this->hora_inicio;
+}
+public function setHoraFin($var){
+     $this->hora_fin=$var;
+}
+public function getHoraFin(){
+	return $this->hora_fin;
+}
+public function setHoraIdHorario($var){
+     $this->horario_id_horario=$var;
+}
+public function getHoraIdHorario(){
+	return $this->hora_id_horario;  
+}
+    
+}
+?>
