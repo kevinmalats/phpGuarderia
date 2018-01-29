6a5e26d0a6bff5acdcb7a94d2f4e49011acc0a6d
Kevin <alexaortepe1989@gmail.com>
2018-01-28 21:23:53 -0500

    Agregando clase y colector ciudad

---
+++ src/ciudad.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* 
+*/
+class Ciudad {
+
+private $id_ciudad;
+private $descripcion ;
+
+
+
+public function __construct(){
+   
+
+}
+public function setId($id_usuario){
+     $this->id_ciudad=$id_usuario;
+}
+public function getId(){
+	return $this->id_ciudad;
+}
+public function setNombre($var){
+     $this->descripcion=$var;
+}
+public function getNombre(){
+	return $this->descripcion;
+}
+
+
+}
---
+++ src/collectorCiudad.php
@@ -0,0 +1,55 @@
+<?php
+
+include_once('ciudad.php');
+include_once('database/collector.php');
+
+class CiudadCollector extends Collector
+{
+  
+  function showCiudades() {
+    $rows = self::$db->getRows("SELECT * FROM ciudad ");        
+    $arrayUsuario= array();        
+    foreach ($rows as $c){
+      $aux = new Ciudad();
+      $aux->setId($c{'id_ciudad'});
+      $aux->setNombre($c{'descripcion'});
+     
+      array_push($arrayUsuario, $aux);
+    }
+    return $arrayUsuario;        
+  }
+  
+function showCiudad($id) {
+    $rows = self::$db->getRows("SELECT * FROM ciudad where id_ciudad= ? ", array ("{$id}"));        
+$ObjUsuario= new Ciudad();
+$ObjUsuario->setId($rows[0]{'id_ciudad'});
+$ObjUsuario->setNombre($rows[0]{'descripcion'});
+
+    
+    return $ObjUsuario;        
+  }
+
+
+function updateCiudad($id_ciudad,$descripcion) {
+
+      $insertrow = self::$db->updateRow("UPDATE public.ciudad SET descripcion = ? where id_ciudad= ? ", array ("{$descripcion}",$id_ciudad));
+
+}
+
+function deleteCiudad($id) {
+    $deleterow = self::$db->deleteRow("DELETE FROM public.ciudad where id_ciudad= ? ", array ("{$id}"));
+
+}
+function insertCiudad($descripcion) {
+  
+  $rows = self::$db->insertRow("INSERT INTO public.ciudad(descripcion) VALUES (?) returning id_ciudad", array ("{$descripcion}"));      
+    return $rows{"id_ciudad"} ;      
+    
+  
+  }
+
+
+
+
+}
+?>
