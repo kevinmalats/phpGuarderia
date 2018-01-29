1824dd7bcb3fc3bec7d63a8a639446fe602be9cd
Kevin <ken_1721@hotmail.es>
2018-01-28 20:21:13 -0500

    cambios en el crud

--- src/database/collector.php
+++ src/database/collector.php
@@ -2,16 +2,16 @@
 include_once('database.php');
 
 // Define configuration
-define("DB_HOST", "ec2-23-23-110-26.compute-1.amazonaws.com");
+/*define("DB_HOST", "ec2-23-23-110-26.compute-1.amazonaws.com");
 define("DB_USER", "gcsmegnwpekghd");
 define("DB_PASS", "5b2b53bc89ee99edb82a8bfd1953b764dd9d870aca60d2918b7876687ff97625");
 define("DB_NAME", "d2l7s76r7g7gs6");
-
-/*define("DB_HOST", "localhost");
+*/
+define("DB_HOST", "localhost");
 define("DB_USER", "postgres");
 define("DB_PASS", "1234");
 define("DB_NAME", "guarderia");
-*/
+
 
 
 
