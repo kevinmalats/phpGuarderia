dc35aa847247c63fd464108e0d33fbd481d8a246
davidrobelly <david.robelly@gmail.com>
2018-02-05 16:53:49 -0500

    correciones en clases php

--- admin/infoUsuario/index.php
+++ admin/infoUsuario/index.php
@@ -32,7 +32,7 @@ require_once("../../src/collectorInformacionUsuario.php");
     echo "<div class='container-fluid'>";
     echo "<div class='navbar-header'><a class='navbar-brand' >Tabla Usuario</a></div>";
     echo " <ul class='nav navbar-nav'>";
-		      	echo "<li><a href='../index.php'>Menú</a></li>";
+		      	echo "<li><a href='../index.php'>Atrás</a></li>";
 			    echo "<li><a href='crear.php'>Nuevo</a></li>";
 		echo "</ul>";
     echo " <ul class='nav navbar-nav navbar-right minav'>";
