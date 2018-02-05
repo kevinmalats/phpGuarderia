64851fe8b90b85bea6827d2e07c4eea6453ce882
kevinmalats <ken_1721@hotmail.es>
2018-02-05 09:35:55 -0500

    probando carga de imagenes

--- admin/infoUsuario/guardar.php
+++ admin/infoUsuario/guardar.php
@@ -3,13 +3,12 @@
 if(isset($_POST)){
 	require_once("../../src/collectorInformacionUsuario.php"); 
 $objColl= new InformacionUsuarioCollector();
-    $dir_subida = "/var/www/html/phpGuarderia/img/perfil/";
+$dir_subida = "../../img/perfil/";
 $fichero_subido = $dir_subida.basename($_FILES['archivo']['name']."");
 $fotoperfil=$_SERVER_DOCUMENT['root']."img/perfil/".basename($_FILES['archivo']['name']."");
 echo $fotoperfil;
 echo '<pre>';
- if(is_uploaded_file($_FILES['archivo']['tmp_name'])) {
- 	copy($_FILES['archivo']['tmp_name'], $fichero_subido);
+if(move_uploaded_file($_FILES['archivo']['tmp_name'], $fichero_subido))  {
                              
    $nombres=$_POST["nombres"];
    $apellidos=$_POST["apellidos"];
@@ -17,7 +16,7 @@ echo '<pre>';
    $id_usuario=$_POST["usuario"];
    $direccion=$_POST["direccion"];
   $objColl->insertInfoUsuario($nombres, $apellidos,$fotoperfil ,$correo,$id_usuario,$direccion);
-   echo "<meta http-equiv='refresh' content='0;URL= crear.php?mensaje= Se creó exitosamente'>";
+   //echo "<meta http-equiv='refresh' content='0;URL= crear.php?mensaje= Se creó exitosamente'>";
    //  header("location: crear.php?mensaje= Se creó exitosamente");
  } else
     print_r($_FILES);
